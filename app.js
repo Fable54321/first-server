f3d60261f93382bef0a15a493b81c9823afd5167
Timothe Bissonnette <timothe5@hotmail.com>
2025-04-08 11:00:25 -0400

    first commit

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage,
   limits: {
-    fileSize: 1024 * 1024 * 25 // 25 MB
+    fileSize: 1024 * 1024 * 10 // 25 MB
   }
  });
 
